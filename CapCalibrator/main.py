305c3492d702d8ef422f20063735fe6c5cc9924e
yoterel <erelyotam@gmail.com>
2020-09-22 10:37:51 +0300

    changes "model" cmd line argument to "template"

--- CapCalibrator/main.py
+++ CapCalibrator/main.py
@@ -10,8 +10,8 @@ from file_io import save_results
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Calibrates fNIRS sensors location based on video.')
     parser.add_argument("video", help="The path to the video file to calibrate sensors with.")
-    parser.add_argument("model", help="The base model file path given in space delimited csv format of size nx3.")
-    parser.add_argument("-gt", "--ground_truth", help="The ground truth file path to compare results to given in space delimited csv format of size nx3.")
+    parser.add_argument("template", help="The template file path (given in space delimited csv format of size nx3).")
+    parser.add_argument("-gt", "--ground_truth", help="The ground truth file path to compare results to (given in space delimited csv format of size nx3).")
     parser.add_argument("-m", "--mode", type=str, choices=["manual", "semi-auto", "auto", "special"],
                         default="semi-auto",
                         help="Controls whether to automatically or manually annotate the stickers in the video.")
@@ -20,20 +20,20 @@ def parse_arguments():
     # if len(sys.argv) == 1:
     #     parser.print_help(sys.stderr)
     #     sys.exit(1)
-    cmd_line = 'E:/University/masters/CapTracking/videos/telaviv/yaara/session1/1.MP4 E:/Src/CapCalibrator/example_models/example_model2.txt -gt E:/University/masters/CapTracking/videos_data/yaara -m special -v 1'.split()
+    cmd_line = 'E:/University/masters/CapTracking/videos/telaviv/aviv E:/Src/CapCalibrator/example_models/example_model3.txt -gt E:/Src/CapCalibrator/example_models/telaviv_experiment/aviv.txt -m special -v 1'.split()
     # cmd_line = '/disk1/yotam/capnet/openPos/openPos55/GX011592.MP4 /disk1/yotam/capnet/openPos/openPos/openPos50 -m special -gt /disk1/yotam/capnet/openPos/openPos55'.split()
     # cmd_line = '/disk1/yotam/capnet/openPos/real_babies/1778b/GX011447.MP4 /disk1/yotam/capnet/openPos/openPos/openPos50 -m manual -v 1'.split()
     args = parser.parse_args(cmd_line)
     args.video = Path(args.video)
     # if Path.is_dir(args.video):
     #     args.video = args.video.glob("*.MP4").__next__()
-    args.model = Path(args.model)
-    if Path.is_dir(args.model):
-        args.model = args.model.glob("*.txt").__next__()
+    args.template = Path(args.template)
+    if Path.is_dir(args.template):
+        args.template = args.template.glob("*.txt").__next__()
     if args.ground_truth:
         args.ground_truth = Path(args.ground_truth)
-        # if Path.is_dir(args.ground_truth):
-        #     args.ground_truth = args.ground_truth.glob("*.txt").__next__()
+        if Path.is_dir(args.ground_truth):
+            args.ground_truth = args.ground_truth.glob("*.txt")
     return args
 
 
@@ -41,12 +41,7 @@ if __name__ == "__main__":
     args = parse_arguments()
     sticker_locations = video.process_video(args)  # nx10x14 floats
     r_matrix, s_matrix = predict.predict_rigid_transform(sticker_locations, args)
-    sensor_locations = geometry.apply_rigid_transform(r_matrix,
-                                                      s_matrix,
-                                                      args.model,
-                                                      args.ground_truth,
-                                                      plot=True,
-                                                      v=args.verbosity)
+    sensor_locations = geometry.apply_rigid_transform(r_matrix, s_matrix, args, plot=True)
     projected_data = geometry.project_sensors_to_MNI(sensor_locations, args.verbosity)
     save_results(projected_data, args.output_file, args.verbosity)
     if args.verbosity:
